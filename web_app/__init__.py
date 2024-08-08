44a1e6a6fd027ba03f6cc0bddf5119c8fdc23250
hyacoub98 <hzy208@stern.nyu.edu>
2024-08-08 19:51:42 -0400

    fix recipe routes

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -2,13 +2,12 @@
 
 from flask import Flask
 
-from web_app.routes.home_routes import home_routes
-from web_app.routes.stocks_routes import stocks_routes
+from web_app.routes.recipe_routes import recipe_routes
+
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(home_routes)
-    app.register_blueprint(stocks_routes)
+    app.register_blueprint(recipe_routes)
     return app
 
 if __name__ == "__main__":
